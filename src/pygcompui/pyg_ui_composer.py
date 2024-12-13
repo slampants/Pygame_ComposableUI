43ef792d2c0528c6bfab4f3a230cea7f8dc2a6fa
Sam Friedman <7559929+slampants@users.noreply.github.com>
2024-12-13 16:22:07 -0500

    Restructuring for packaging

---
+++ src/pygcompui/pyg_ui_composer.py
@@ -0,0 +1,6 @@
+class PygUIComposer:
+    """
+    The Pygame UI Composer is an interface for creating and getting UI elements and screens to be displayed by pygame
+    """
+
+    def __init__(self, screen_width: int, screen_height: int, ):
